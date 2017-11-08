ff8572281ce16d3682a2c02ec4c072d5c68fc4ec
Roberto Achar <robertoachar@gmail.com>
2017-11-08 13:02:23 -0200

    feature: add express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('It works');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'It works!' });
+});
+
+app.listen(port, () => {
+  console.info(`Express is running on port: ${port}`);
+});
